1f6b179577341ab37c50fa0a7f771f474d7c9af0
victorgarcia00 <victor.garcia@juanxxiii.net>
2022-02-11 19:41:41 +0100

    ns que intento

--- src/clase1.java
+++ src/clase1.java
@@ -4,6 +4,8 @@ public class clase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola jaime");
+		System.out.println("hola liviu");
+
 	}
 
 }
